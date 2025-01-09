c2034b0a68e2d46c464ca47bf6e8812357f28f5f
RahulPB08 <r.p.bhosle08@gmail.com>
2025-01-09 20:14:12 +0530

    changes realted accomodation

---
+++ src/jsx/Accommodation.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../css/Accomodation.css"
+import NavBar from './NavBar'
+
+const Accommodation = () => {
+  return (
+    <div className='Accommodation'>
+        <div className='AccomodationNavBar'>
+            <NavBar className="AccomodationNavBar" />
+        </div>
+      <h1 className='h1class'>Coming Soon....</h1>
+    </div>
+  )
+}
+
+export default Accommodation
---
+++ src/main.jsx
@@ -0,0 +1,56 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import FirstPage from './jsx/HomePage';
+import 'remixicon/fonts/remixicon.css';
+import EventPage from './jsx/EventPage';
+import Contact from './jsx/Contact';
+import Loader from './jsx/Loader';
+import Accommodation from './jsx/Accommodation';
+import { useState, useEffect, useRef } from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <FirstPage />,
+  },
+  {
+    path: "/event",
+    element: <Accommodation />,
+  },
+  {
+    path: "/accomodation",
+    element: <Accommodation />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  }
+]);
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const hasLoadedOnce = useRef(false); 
+
+  useEffect(() => {
+    if (!hasLoadedOnce.current) {
+      setTimeout(() => {
+        setLoading(false);
+        hasLoadedOnce.current = true; 
+      }, 4000); 
+    } else {
+      setLoading(false); 
+    }
+  }, []);
+
+  return (
+    <StrictMode>
+      {loading ? <Loader /> : <RouterProvider router={router} />}
+    </StrictMode>
+  );
+}
+
+createRoot(document.getElementById('root')).render(<App />);
