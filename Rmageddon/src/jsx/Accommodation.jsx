7a9dad3817ad08a45102fb2d2b8478b49aba6f98
Ayush Mahajan <140263932+Ayush-AM@users.noreply.github.com>
2025-01-10 11:35:31 +0530

    aos updated, and explore button changed

--- Rmageddon/src/jsx/Accommodation.jsx
+++ Rmageddon/src/jsx/Accommodation.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import "../css/Accomodation.css"
 import NavBar from './NavBar'
 import Footer from './Footer'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-const Accommodation = () => {
+const Accommodation = ({ className }) => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    });
+  }, []);
   return (
     <>
     <div className='Accommodation'>
         <div className='AccomodationNavBar'>
-            <NavBar className="AccomodationNavBar" />
+            <NavBar className="AccomodationNavBar" data-aos="fade-down"/>
         </div>
-      <h1 className='h1class'>Coming Soon....</h1>
+      <h1 className='h1class' data-aos="fade-right">Coming Soon....</h1>
     </div>
     <Footer />
     </>
